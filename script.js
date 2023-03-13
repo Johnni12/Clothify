b2a5f84ea848f38a22eba53bc5b7cb895c371b24
kalu john <kalu.johnagu@gmail.com>
2023-03-14 00:55:45 +0100

    Header initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict'
+const navLinks = document.getElementById('navLinks')
+
+const showMenu = () => {
+  navLinks.style.right = '0px'
+}
+
+const hideMenu = () => {
+  navLinks.style.right = '-200px'
+}
